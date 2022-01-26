7350d974cf9ae9a7709440cb494aa02575e4033f
Kevin Frei <kevinfrei@hotmail.com>
2022-01-26 10:42:55 -0800

    3 different tests loop nicely now

--- test.cpp
+++ test.cpp
@@ -10,10 +10,10 @@ const uint8_t TFT_DC = 15;
 const uint8_t TFT_RST = 6;
 
 Adafruit_ST7789 tft(TFT_CS, TFT_DC, TFT_RST);
+
 boolean backlight_on = false;
 
-uint32_t start = 0;
-uint32_t next_task = 0;
+const char* rot_names[] = {"0:Port", "1:Land", "2!Port", "3!Land"};
 
 void set_backlight(bool turnOn) {
   if (backlight_on != turnOn) {
@@ -21,6 +21,49 @@ void set_backlight(bool turnOn) {
     backlight_on = turnOn;
   }
 }
+
+// Display the name of the current orientation
+void printOrientation(uint8_t rot) {
+  int16_t x, y;
+  uint16_t w, h;
+  tft.setCursor(0, 0);
+  const char* str = rot_names[rot];
+  tft.getTextBounds(str, 0, 0, &x, &y, &w, &h);
+  uint16_t xx = (tft.width() - w) / 2;
+  uint16_t yy = (tft.height() + h) / 2;
+  tft.setCursor(xx, yy);
+  tft.fillRoundRect(xx + x - 10, yy + y - 10, w + 21, h + 21, 5, ST77XX_RED);
+  tft.fillRoundRect(xx + x - 3, yy + y - 3, w + 6, h + 6, 2, ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print(str);
+}
+
+void clearScreenTest(uint8_t rot) {
+  tft.fillScreen(ST77XX_BLUE);
+  tft.setRotation(rot);
+  tft.fillScreen(ST77XX_BLACK);
+}
+
+void rectFrameTest(uint8_t rot) {
+  tft.fillScreen(ST77XX_BLACK);
+  delay(10);
+  tft.setRotation(rot);
+  tft.fillScreen(ST77XX_GREEN); // I'm worried that this won't expose
+                                // other issues...
+  tft.fillRect(1, 1, tft.width() - 2, tft.height() - 2, ST77XX_BLACK);
+}
+
+void cornerDotsTest(uint8_t rot) {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setRotation(rot);
+  tft.drawPixel(0, 0, ST77XX_WHITE);
+  tft.drawPixel(0, tft.height() - 1, ST77XX_WHITE);
+  tft.drawPixel(tft.width() - 1, 0, ST77XX_WHITE);
+  tft.drawPixel(tft.width() - 1, tft.height() - 1, ST77XX_WHITE);
+}
+
+uint32_t start = 0;
+uint32_t next_task = 0;
 void setup() {
   pinMode(BACKLIGHT_PIN, OUTPUT);
   set_backlight(true);
@@ -32,48 +75,33 @@ void setup() {
   start = millis();
   next_task = start + 10;
 }
+
 uint8_t cur_rot = 0;
-bool fill_not_rect = false;
-const char* rot_names[] = {
-  "0:Port", "1:Land", "2!Port", "3!Land"};
+uint8_t which_test = 0;
+typedef void (*test)(uint8_t);
+const test tests[] = {clearScreenTest, rectFrameTest, cornerDotsTest};
+const uint8_t num_tests = sizeof(tests) / sizeof(tests[0]);
+
 void loop() {
   uint32_t now = millis();
   if (now >= next_task) {
     // You can speed it up or slow it down here
-    // Teensy 4.0 can do this wicket fast :)
-    next_task = now + 1500; // Run this every 1.5 seconds
+    // Teensy 4.0 can do this wicked fast :)
+    next_task = now + 1000; // Run a new task every 1 second
     tft.setRotation(0);
-    if (fill_not_rect) {
-      tft.fillScreen(ST77XX_BLUE);
-      tft.setRotation(cur_rot);
-      tft.fillScreen(ST77XX_BLACK);
-    } else {
-      tft.fillScreen(ST77XX_BLACK);
-      delay(10);
-      tft.setRotation(cur_rot);
-      tft.fillScreen(ST77XX_GREEN); // I'm worried that this won't expose other
-                                    // issues...
-      tft.fillRect(1, 1, tft.width() - 2, tft.height() - 2, ST77XX_BLACK);
-    }
-    int16_t x, y;
-    uint16_t w, h;
-    tft.setCursor(0, 0);
-    const char* str = rot_names[cur_rot];
-    tft.getTextBounds(str, 0, 0, &x, &y, &w, &h);
-    uint16_t xx = (tft.width() - w) / 2;
-    uint16_t yy = (tft.height() + h) / 2;
-    tft.setCursor(xx, yy);
-    tft.fillRoundRect(xx + x - 10, yy + y - 10, w + 21, h + 21, 5, ST77XX_RED);
-    tft.fillRoundRect(xx + x - 3, yy + y - 3, w + 6, h + 6, 2, ST77XX_BLACK);
-    tft.setTextColor(ST77XX_WHITE);
-    tft.print(str);
+    // Run the test
+    tests[which_test](cur_rot);
+    // Print the current orientation in the middle
+    printOrientation(cur_rot);
+    // Go to a new orientation
     cur_rot = (cur_rot + 1) & 3;
+    // If we've looped around, pick a new test
     if (!cur_rot) {
-      fill_not_rect = !fill_not_rect;
+      which_test = (which_test + 1) % num_tests;
     }
   }
-  // Turn off the backlight after 1 minute
-  if (now - start > 1 * 60 * 1000) {
+  // Turn off the backlight after 5 minutes
+  if (now - start > 5 * 60 * 1000) {
     set_backlight(false);
   }
 }
